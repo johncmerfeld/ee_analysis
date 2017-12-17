7c2310aea25a5d34949e897b47e7bedd82e09600
John Merfeld <john.merfeld@gmail.com>
2017-12-16 20:38:02 -0600

    commented code

--- ee_visualize.R
+++ ee_visualize.R
@@ -2,7 +2,13 @@ setwd("Documents/Code/ee_analysis/")
 library(ggplot2)
 library(data.table)
 library(reshape2)
+library(scales)
+
+# load in data, coerce to date format
 ee_data <- fread("ee_data.csv")
+ee_data$date = as.Date(ee_data$date)
+
+# add cumulative sums from count data
 ee_data <- within(ee_data, Hypothermia <- cumsum(hypo_plays))
 ee_data <- within(ee_data, Sparks <- cumsum(sparks_plays))
 ee_data <- within(ee_data, Moth <- cumsum(moth_plays))
@@ -17,17 +23,20 @@ ee_data <- within(ee_data, Dime <- cumsum(dime_plays))
 ee_data <- within(ee_data, October <- cumsum(oct_plays))
 ee_data <- within(ee_data, Later <- cumsum(later_plays))
 
-ee_totals <- ee_data[22:60,c("date","Hypothermia","Sparks","Moth","Sail",
-                             "Messier","Corpses","ESA","Dakin","BreGlea",
+# ignore Moth single
+ee_totals <- ee_data[21:60,c("date","Hypothermia","Sparks","Moth","Sail",
+                             "Messier","Corpses","ESA","Dakin","BrenGlea",
                              "Debt","Dime","October","Later")]
 
+# reshape and plot
 ee_melt <- melt(ee_totals,id="date")
 ggplot(ee_melt,aes(x=date,
                    y=value,
                    colour=variable,
                    group=variable)) + 
- geom_line() + 
+ geom_line(size = 1.5) + 
  labs(color = "Song",
        title = "EE song performance over time",
        x = "Date",
-       y = "Total listens")
+       y = "Total listens") +
+  scale_x_date(breaks = date_breaks(width = "1 week"))
